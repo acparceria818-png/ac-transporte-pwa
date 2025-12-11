212aa670b78e9326ad4666e747bc6344d71481f6
Juan-AC <acparceria818@gmail.com>
2025-12-11 07:55:48 -0300

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE = "ac-transporte-v2";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/logo.jpg",
+  "/logo.png",
+  "/avatar.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
